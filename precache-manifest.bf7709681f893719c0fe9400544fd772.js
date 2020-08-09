11e56ab957cc16a6372748a41247b1e3182a25b5
Dálcio <dalciomacuetegarcia@gmail.com>
2020-08-10 00:13:30 +0100

    Updates

---
+++ precache-manifest.bf7709681f893719c0fe9400544fd772.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01645620ab1e322c23a684a8ae0bf809",
+    "url": "/dalciogarcia/index.html"
+  },
+  {
+    "revision": "f2f79081cff8ad609cf2",
+    "url": "/dalciogarcia/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "3c400ccb5d1942339fa1",
+    "url": "/dalciogarcia/static/css/main.d676b68f.chunk.css"
+  },
+  {
+    "revision": "f2f79081cff8ad609cf2",
+    "url": "/dalciogarcia/static/js/2.b557d7df.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/dalciogarcia/static/js/2.b557d7df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c400ccb5d1942339fa1",
+    "url": "/dalciogarcia/static/js/main.c2fcaf90.chunk.js"
+  },
+  {
+    "revision": "098e18c3e8cb437c934c",
+    "url": "/dalciogarcia/static/js/runtime-main.1b4fe5c8.js"
+  },
+  {
+    "revision": "3aa75db7b4805b9bb58f43d17d98122c",
+    "url": "/dalciogarcia/static/media/background.3aa75db7.jpg"
+  },
+  {
+    "revision": "4368568b3881a202cfd48472a27ee131",
+    "url": "/dalciogarcia/static/media/dalcio-260w.4368568b.jpg"
+  }
+]);
