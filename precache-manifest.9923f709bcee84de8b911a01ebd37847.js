1b46353e15d3ab90e2fb6b87445dc31f82eb0031
Dálcio <dalciomacuetegarcia@gmail.com>
2020-08-12 02:51:19 +0100

    Updates

--- precache-manifest.9923f709bcee84de8b911a01ebd37847.js
+++ precache-manifest.9923f709bcee84de8b911a01ebd37847.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0a41f35c308c6b34844d24ee63db2683",
+    "revision": "4ca20a884c77a9ca55b4fa74ea8d4b3a",
     "url": "/dalciogarcia/index.html"
   },
   {
-    "revision": "78a44cff7a94b1d4eb99",
+    "revision": "c736d386c5134e62343b",
     "url": "/dalciogarcia/static/css/2.4819d237.chunk.css"
   },
   {
-    "revision": "be3ee13a8c99acc8c4e9",
+    "revision": "76bc033a1b823d3a1c01",
     "url": "/dalciogarcia/static/css/main.a852e211.chunk.css"
   },
   {
-    "revision": "78a44cff7a94b1d4eb99",
-    "url": "/dalciogarcia/static/js/2.80a95dde.chunk.js"
+    "revision": "c736d386c5134e62343b",
+    "url": "/dalciogarcia/static/js/2.3a6fd1a5.chunk.js"
   },
   {
     "revision": "c87550333da8df8ab4c80bd1d6e9defb",
-    "url": "/dalciogarcia/static/js/2.80a95dde.chunk.js.LICENSE.txt"
+    "url": "/dalciogarcia/static/js/2.3a6fd1a5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "be3ee13a8c99acc8c4e9",
-    "url": "/dalciogarcia/static/js/main.ea7f0f6d.chunk.js"
+    "revision": "76bc033a1b823d3a1c01",
+    "url": "/dalciogarcia/static/js/main.670cce4b.chunk.js"
   },
   {
     "revision": "098e18c3e8cb437c934c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dalciogarcia/precache-manifest.fcc558eb6b83c3da2966eed93bdcabd1.js"
+  "/dalciogarcia/precache-manifest.9923f709bcee84de8b911a01ebd37847.js"
 );
 
 self.addEventListener('message', (event) => {
